d752fde06a19e07c5f74a8baab0fc84cd30f1e43
Rizky Kharisma <r.kharisma29@gmail.com>
2023-01-26 19:30:32 +0700

    Refactor: Inject to data and domain layer directly from hilt (2)

---
+++ app/src/main/java/com/ngengs/android/app/dailyimage/di/ApplicationModule.kt
@@ -0,0 +1,39 @@
+package com.ngengs.android.app.dailyimage.di
+
+import android.content.Context
+import com.ngengs.android.app.dailyimage.data.local.DailyImageDatabase
+import com.ngengs.android.app.dailyimage.data.remote.UnsplashAPI
+import com.ngengs.android.app.dailyimage.data.remote.UnsplashPublicAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Created by rizky.kharisma on 25/01/23.
+ * @ngengs
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object ApplicationModule {
+    @Singleton
+    @Provides
+    fun provideAPI(
+        @ApplicationContext context: Context
+    ): UnsplashAPI = UnsplashAPI.instantiate(context)
+
+    @Singleton
+    @Provides
+    fun providePublicAPI(
+        @ApplicationContext context: Context
+    ): UnsplashPublicAPI = UnsplashPublicAPI.instantiate(context)
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): DailyImageDatabase = DailyImageDatabase.initialize(context)
+}
