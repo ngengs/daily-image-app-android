6f600ba00ab342b4ccc51b0dcb250ffbc5ec4943
Rizky Kharisma <r.kharisma29@gmail.com>
2023-01-26 20:54:25 +0700

    Test: Add test for favorite repository (2)

--- app/src/test/java/com/ngengs/android/app/dailyimage/helpers/fake/data/source/FakePhotoLocalDataSource.kt
+++ app/src/test/java/com/ngengs/android/app/dailyimage/helpers/fake/data/source/FakePhotoLocalDataSource.kt
@@ -2,6 +2,10 @@ package com.ngengs.android.app.dailyimage.helpers.fake.data.source
 
 import com.ngengs.android.app.dailyimage.data.local.model.PhotosLocal
 import com.ngengs.android.app.dailyimage.data.source.PhotoLocalDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 /**
  * Created by rizky.kharisma on 14/01/23.
@@ -10,6 +14,8 @@ import com.ngengs.android.app.dailyimage.data.source.PhotoLocalDataSource
 class FakePhotoLocalDataSource : PhotoLocalDataSource {
     var popularPhotos: MutableList<PhotosLocal> = mutableListOf()
     var latestPhotos: MutableList<PhotosLocal> = mutableListOf()
+    val favoritePhotos: MutableList<PhotosLocal> = mutableListOf()
+    val favoriteFlow: MutableStateFlow<List<PhotosLocal>> = MutableStateFlow(emptyList())
 
     override suspend fun getPopular(): List<PhotosLocal> =
         popularPhotos.toList()
@@ -32,8 +38,26 @@ class FakePhotoLocalDataSource : PhotoLocalDataSource {
         latestPhotos.clear()
     }
 
+    override fun getFavorites(): Flow<List<PhotosLocal>>  = favoriteFlow.asSharedFlow()
+        .distinctUntilChanged()
+
+    override suspend fun getFavorite(id: String): PhotosLocal? {
+        return favoritePhotos.find { it.id == id }
+    }
+
+    override suspend fun saveFavorite(data: PhotosLocal) {
+        favoritePhotos.add(0, data)
+        favoriteFlow.emit(favoritePhotos.toList())
+    }
+
+    override suspend fun deleteFavorite(data: PhotosLocal) {
+        val removed = favoritePhotos.removeIf { it.id == data.id }
+        if (removed) favoriteFlow.emit(favoritePhotos.toList())
+    }
+
     fun reset() {
         popularPhotos.clear()
         latestPhotos.clear()
+        favoritePhotos.clear()
     }
 }
