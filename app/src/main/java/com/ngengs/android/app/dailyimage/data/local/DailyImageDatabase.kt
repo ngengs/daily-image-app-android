c2e07ae36605eb332fd9ce2e72146ea6e7bc0f4c
Rizky Kharisma <r.kharisma29@gmail.com>
2023-02-01 00:17:33 +0700

    Feat: Use auto migration for favorite feature

--- app/src/main/java/com/ngengs/android/app/dailyimage/data/local/DailyImageDatabase.kt
+++ app/src/main/java/com/ngengs/android/app/dailyimage/data/local/DailyImageDatabase.kt
@@ -2,12 +2,11 @@ package com.ngengs.android.app.dailyimage.data.local
 
 import android.content.Context
 import androidx.annotation.VisibleForTesting
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.ngengs.android.app.dailyimage.data.local.model.FavoritePhotos
 import com.ngengs.android.app.dailyimage.data.local.model.LatestPhotos
 import com.ngengs.android.app.dailyimage.data.local.model.PhotosLocal
@@ -25,14 +24,15 @@ import com.ngengs.android.app.dailyimage.data.local.model.PopularPhotos
         FavoritePhotos::class
     ],
     version = DailyImageDatabase.VERSION,
-    exportSchema = false
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
 )
 @TypeConverters(DailyImageDatabaseTypeConverter::class)
 abstract class DailyImageDatabase : RoomDatabase() {
     abstract fun photosDao(): PhotosDao
     abstract fun popularDao(): PopularPhotosDao
     abstract fun latestDao(): LatestPhotosDao
-
     abstract fun favoriteDao(): FavoritePhotosDao
 
     companion object {
@@ -50,17 +50,6 @@ abstract class DailyImageDatabase : RoomDatabase() {
 
         fun initialize(context: Context) =
             Room.databaseBuilder(context, DailyImageDatabase::class.java, NAME)
-                .addMigrations(MIGRATION_1_2)
                 .build()
-
-        private val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("CREATE TABLE `$TABLE_FAVORITE` (" +
-                        "`$COLUMN_ID` INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        "`$COLUMN_PHOTO_ID` TEXT NOT NULL" +
-                        ");")
-                database.execSQL("CREATE UNIQUE INDEX index_${TABLE_FAVORITE}_${COLUMN_PHOTO_ID} ON $TABLE_FAVORITE ($COLUMN_PHOTO_ID)")
-            }
-        }
     }
 }
