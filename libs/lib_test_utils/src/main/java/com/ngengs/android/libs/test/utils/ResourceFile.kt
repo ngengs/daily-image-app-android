67acc0b4a669c135d044985ddbb81d2f2c92a4bf
Rizky Kharisma <r.kharisma29@gmail.com>
2023-02-03 09:47:36 +0700

    CI: Fix lint

--- libs/lib_test_utils/src/main/java/com/ngengs/android/libs/test/utils/ResourceFile.kt
+++ libs/lib_test_utils/src/main/java/com/ngengs/android/libs/test/utils/ResourceFile.kt
@@ -6,14 +6,11 @@ import java.io.File
  * Created by rizky.kharisma on 29/12/22.
  * @ngengs
  */
-object ResourceFile{
+object ResourceFile {
     fun getJson(path: String): String {
         // Load the JSON response
         val uri = this.javaClass.classLoader?.getResource("json/$path")
         val file = uri?.path?.let { File(it) }
         return file?.let { String(it.readBytes()) }.orEmpty()
     }
-
-
-
 }
